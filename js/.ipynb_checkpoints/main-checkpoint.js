166b239f9a1f47ed1a9b3c3e34c63ee0731fb807
willylulu <maya6282@gmail.com>
2018-06-11 02:00:02 +0800

    update

---
+++ js/.ipynb_checkpoints/main-checkpoint.js
@@ -0,0 +1,39 @@
+$.fn.extend({
+  animateCss: function(animationName, callback) {
+    var animationEnd = (function(el) {
+      var animations = {
+        animation: 'animationend',
+        OAnimation: 'oAnimationEnd',
+        MozAnimation: 'mozAnimationEnd',
+        WebkitAnimation: 'webkitAnimationEnd',
+      };
+
+      for (var t in animations) {
+        if (el.style[t] !== undefined) {
+          return animations[t];
+        }
+      }
+    })(document.createElement('div'));
+
+    this.addClass('animated ' + animationName).one(animationEnd, function() {
+      $(this).removeClass('animated ' + animationName);
+
+      if (typeof callback === 'function') callback();
+    });
+
+    return this;
+  },
+});
+
+$("[href='#seg2']").click(function(){
+    $("#avatar").animateCss('bounceInLeft');
+    $("#links").animateCss('bounceInLeft');
+});
+
+$("#links div").hover(function(){
+    $(this).animateCss("tada");
+});
+
+$("#avatar").hover(function(){
+    $(this).animateCss("shake");
+});
